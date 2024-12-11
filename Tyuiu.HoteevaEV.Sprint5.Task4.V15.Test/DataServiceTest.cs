8ea17868161a87e19d700a8809c182f9beeb2870
Tyuiu.HoteevaEV@outlook.com <Tyuiu.HoteevaEV@outlook.com>
2024-12-11 22:26:56 +0500

    Task4

---
+++ Tyuiu.HoteevaEV.Sprint5.Task4.V15.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.HoteevaEV.Sprint5.Task4.V15.Lib;
+using System.IO;
+namespace Tyuiu.HoteevaEV.Sprint5.Task4.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask4V15.txt";
+
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileexists = fileinfo.Exists;
+            Assert.AreEqual(true, fileexists);
+        }
+    }
+}
---
+++ Tyuiu.HoteevaEV.Sprint5.Task4.V15/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.HoteevaEV.Sprint5.Task4.V15.Lib;
+namespace Tyuiu.HoteevaEV.Sprint5.Task4.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Хотеева Е. В. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнила: Хотеева Елена Вячеславовна | ИИПб-24-2                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Прочитать значение из файла и подставить в формулу. Вычислить значение  *");
+            Console.WriteLine("* выражения по формуле и вернуть результат на консоль.                    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask4V15.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
